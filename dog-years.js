6d193e9d1e9e0d5cc3878bdb173bc67800d8d1bc
yelhamdi <yassin890890.890@gmail.com>
2025-11-26 15:12:33 +0100

    10-step

---
+++ dog-years.js
@@ -0,0 +1,15 @@
+ const earth = 31557600 
+
+const  mercury = earth*0.2408467 
+ const venus = earth*0.61519726 
+ const mars = earth*1.8808158 
+ const jupiter = earth*11.862615 
+ const saturn = earth*29.447498 
+const uranus = earth*84.016846 
+ const neptune = earth*164.79132 
+ 
+function dogYears(nameOfPlanet , dogAge){
+    return ((dogAge)/ (nameOfPlanet))*7
+}
+
+console.log(dogYears(earth,1000000000))
