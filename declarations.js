eff0b3da8a0466f8980c625d69e49dead2ebab71
yelhamdi <yassin890890.890@gmail.com>
2025-11-26 13:03:11 +0100

    3-step

---
+++ declarations.js
@@ -0,0 +1,19 @@
+const escapeStr = '\'\\/\"'
+const arr = [4,'2']
+Object.freeze(arr)
+const obj = {
+    str : 'hey' ,
+    num : 1 ,
+    bool : true ,
+    undef : undefined 
+}
+Object.freeze(obj)
+const nested = {
+    arr : [4 , undefined , '2'] ,
+    obj : {
+        str : "hey" ,
+        num : "54" ,
+        bool : true
+    }
+}
+Object.freeze(nested)
