6067aafd72533ca6212fb40866fdd8188c4bc93a
yelhamdi <yassin890890.890@gmail.com>
2025-11-26 17:38:19 +0100

    18-step

--- physics.js
+++ physics.js
@@ -1,9 +1,11 @@
 function getAcceleration(obj){
-    a1 = obj.f/obj.m
-    a2 = obj.Δv/obj.Δt
-    a3 = (2*obj.d)/(obj.t*obj.t)
+    let a1 = obj.f/obj.m
+    let a2 = obj.Δv/obj.Δt
+    let a3 = (2*obj.d)/(obj.t*obj.t)
     if (a1) return a1 ;
     else if (a2) return a2 ;
     else if (a3) return a3 ;
     return 'impossible'
 }
+
+console.log(getAcceleration({}));
