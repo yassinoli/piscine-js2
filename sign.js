58450f1fbdc19b84e277aa823b0b0e3b33c47235
yelhamdi <yassin890890.890@gmail.com>
2025-11-26 16:40:29 +0100

    16-step

--- sign.js
+++ sign.js
@@ -1,8 +1,10 @@
 function sign(a){
-    switch (a){
-        case  (a>0) : return 1 ;
-        case (a<0) : return -1 ;
-        default : return 0
+    if (a>0){
+        return 1
+    }else if (a<0){
+        return -1
+    }else{
+        return 0
     }
 }
 
