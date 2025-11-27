66898e2bf0fe00584287d7364edb23a3fd573d41
yassin elhamdi <yelhamdi@noreply.learn.zone01oujda.ma>
2025-11-27 11:29:42 +0000

    Delete physics.jsphysics.js

--- physics.jsphysics.js
+++
@@ -1,9 +0,0 @@
-function getAcceleration(obj){
-    a1 = obj.f/obj.m
-    a2 = obj.Δv/obj.Δt
-    a3 = 2*obj.d/obj.t**2
-    if (!Number.isNaN(a1)) return a1 ;
-    else if (!Number.isNaN(a2)) return a2 ;
-    else if (!Number.isNaN(a3)) return a3 ;
-    else return "impossible"
-}
