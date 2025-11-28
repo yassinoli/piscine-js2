be4fb647c6372aafa55a5f3b8efb0d15b43ebb1c
yelhamdi <yassin890890.890@gmail.com>
2025-11-28 18:29:36 +0100

    quest-2-8

--- unbreakable.js
+++ unbreakable.js
@@ -1,16 +1,30 @@
-function split(str,elem){
-    let res = []
-     let tmp = ""
-    for (let i=0; i<=str.length ;i++){
-       
-        if (str[i]===elem || i===str.length){
-            res.push(tmp)
-            tmp = ""
-            continue
+function split(str, elem) {
+    let res = [];
+    let tmp = "";
+    for (let i = 0; i <= str.length; i++) {
+        if (str.slice(i, i + elem.length) === elem || i === str.length) {
+            res.push(tmp);
+            tmp = "";
+            if (i < str.length) i += elem.length - 1;
+            continue;
         }
-        tmp += str[i]
+        tmp += str[i];
+    }
+    return res;
+}
 
+
+function join(arr,elem){
+    let res = ""
+    for (let i=0 ; i<arr.length ; i++){
+        if (i<arr.length-1){
+             res += arr[i]+elem
+        }else{
+            res += arr[i]
+        }
     }
     return res
 }
+
+console.log(split("heyhayhey",'yh'));
 
