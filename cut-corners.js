8da078baf15af1b3b3e36188ef31eb520235f171
yelhamdi <yassin890890.890@gmail.com>
2025-11-28 16:44:26 +0100

    quest-2-6

--- cut-corners.js
+++ cut-corners.js
@@ -1,107 +1,51 @@
-function round(x){
-   let intPrt = 0
-    let sgn = 1
-      if (x < 0) {
-        sgn = -1;
-        x = -x;
-    }
-    
-    while (x >= 1) {
-        intPrt++;
-        x -= 1;
-    }
- let  frcPrt = x
-  if (frcPrt >= 0.5) {
-        intPrt++;
-    }
-    
-    return intPrt*sgn
+function intPrt(x) {
+    if (x < 1) return 0;
 
+    let n = 0;
+    let y = x;
+
+    for (let i = 52; i >= 0; i--) {
+        let p = 2 ** i;
+        if (y >= p) {
+            y -= p;
+            n += p;
+        }
+    }
+    return n;
 }
 
-
-
-
-function ceil(x){
-    let intPrt = 0
-    let sgn = 1
-      if (x < 0) {
-        sgn = -1;
-        x = -x;
-    }
-    
-    while (x >= 1) {
-        intPrt++;
-        x -= 1;
-    }
-    
-     if (x === 0) {
-        if( sgn === 1){
-            return intPrt
-        }else{
-            return -intPrt
-        }
-    } else {
-         if( sgn === 1){
-            return intPrt+1
-        }else{
-            return -intPrt
-        }
-    }
-
+function trunc(x) {
+    if (x >= 0) return intPrt(x);
+    return -intPrt(-x);
 }
 
 
 function floor(x) {
-    let intPrt = 0;
-    let sgn = 1;
-
-    if (x < 0) {
-        sgn = -1;
-        x = -x;
-    }
-    
-    while (x >= 1) {
-        intPrt++;
-        x -= 1;
-    }
-
-    if (x === 0) {
-        if( sgn === 1){
-            return intPrt
-        }else{
-            return -intPrt
-        }
+    if (x >= 0) {
+        return intPrt(x);
     } else {
-         if( sgn === 1){
-            return intPrt
-        }else{
-            return -intPrt-1
-        }
+        let ip = intPrt(-x);     
+        if (x === -ip) return -ip;      
+        return -(ip + 1);               
     }
 }
-
-
-
- function trunc(x){
-    let intPrt = 0
-    let sgn = 1
-      if (x < 0) {
-        sgn = -1;
-        x = -x;
+function ceil(x) {
+    if (x >= 0) {
+        let ip = intPrt(x);
+        if (x === ip) return ip;        
+        return ip + 1;                   
+    } else {
+        return -intPrt(-x);     
     }
-    
-    while (x >= 1) {
-        intPrt++;
-        x -= 1;
+}
+function round(x) {
+    if (x >= 0) {
+        let ip = intPrt(x);
+        if (x - ip >= 0.5) return ip + 1;
+        return ip;
+    } else {
+        let ip = intPrt(-x);
+        if ((-x) - ip > -0.5) return -ip;
+        return -(ip + 1);
     }
- 
-
-   return intPrt*sgn
- }
-
-
-
-
-
-
+}
