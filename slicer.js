1c6e2ae122fbb4e12a33975b29415be812975797
yelhamdi <yassin890890.890@gmail.com>
2025-11-28 13:11:26 +0100

    quest-2-4

--- slicer.js
+++ slicer.js
@@ -1,7 +1,21 @@
-function slice(str , start , idx = str.length-1){
-    let res = []
-    for (let i=start ; i<idx ;i++){
-      res.push(str[i])
+function slice(str, start, idx = str.length - 1) {
+  let isString = typeof str === "string";
+  let result;
+
+  if (isString) {
+    result = "";
+  } else {
+    result = [];
+  }
+
+  for (let i = start; i <= idx; i++) {
+    if (isString) {
+      result += str[i];
+    } else {
+      result.push(str[i]);
     }
-    return res
+  }
+
+  return result;
 }
+
