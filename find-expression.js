27dca1cb7f94f869c2d0f39024d896007166c401
yelhamdi <yassin890890.890@gmail.com>
2025-11-29 14:43:21 +0100

    fexp

---
+++ find-expression.js
@@ -0,0 +1,16 @@
+function findExpression(num) {
+    if (num === 1) {
+        return "1";
+    }
+
+    if (num <= 0) {
+        return undefined;
+    }
+
+    if (findExpression(num/2)) {
+        return findExpression(num / 2) + " *2";
+    } else if( findExpression(num -4)) {
+        return findExpression(num - 4) + " +4";
+    }
+}
+
