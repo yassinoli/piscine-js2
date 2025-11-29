03675a1b3998bf4b9bd59b62cad56fe76b868587
yelhamdi <yassin890890.890@gmail.com>
2025-11-29 10:22:17 +0100

    get

--- get.js
+++ get.js
@@ -1,7 +1,13 @@
-function get(src, path) {
-  if (!path) return src;
-  
-  return path.split('.').reduce((obj, key) => {
-    return (obj && obj[key] !== undefined) ? obj[key] : undefined;
-  }, src);
+
+
+function get(src,path){
+    const key = path.split('.')
+    let cur = src
+    for (let ky of key){
+        if (cur == nul || !(ky in current)){
+            return undefined
+        }
+        cur = cur[ky]
+    }
+    return cur 
 }
