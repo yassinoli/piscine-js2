3a0ba6133995144b5df75839fc2b1617da2b384c
yelhamdi <yassin890890.890@gmail.com>
2025-11-30 13:28:44 +0100

    regex4

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,8 @@
-const sameAmount =(str , rgx1 , rgx2)=>{
-    if (str.match(rgx1)&& str.match(rgx2)){
-        return true
-    }
-    return false
-}
+const sameAmount = (str, rgx1, rgx2) => {
+    const count1 = (str.match(rgx1) || []).length;
+    const count2 = (str.match(rgx2) || []).length;
+
+    return count1 === count2;
+};
+
+
