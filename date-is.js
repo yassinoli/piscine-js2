57d4969eac313571451fb705868ff6bbdc670a9f
yelhamdi <yassin890890.890@gmail.com>
2025-12-03 16:23:41 +0100

    date

---
+++ date-is.js
@@ -0,0 +1,23 @@
+function isValid(date) {
+  return date instanceof Date && !isNaN(date.getTime())  && typeof date != 'number'
+}
+
+function isAfter(date1 , date2){
+    return date1 > date2
+}
+function isBefore(){
+    return date1 < date2
+}
+function isFuture(dat){
+    if (!isValid(dat)){
+        return false
+    }
+    return dat.getTime() > Date.now()
+}
+function isPast(dat){
+     if (!isValid(dat)){
+        return false
+    }
+    return dat.getTime() < Date.now()
+}
+
