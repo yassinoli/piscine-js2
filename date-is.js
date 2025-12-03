e5ec86c69e0dc359d2fa6be65e6c64604ef4c074
yelhamdi <yassin890890.890@gmail.com>
2025-12-03 16:33:56 +0100

    date

--- date-is.js
+++ date-is.js
@@ -9,7 +9,7 @@ if (typeof date === "number") {
 function isAfter(date1 , date2){
     return date1 > date2
 }
-function isBefore(){
+function isBefore(date1,date2){
     return date1 < date2
 }
 function isFuture(dat){
