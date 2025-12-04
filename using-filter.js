598e3f8addffff83f5f482f81b7e77aa72f65247
yelhamdi <yassin890890.890@gmail.com>
2025-12-04 17:29:05 +0100

    filter

---
+++ using-filter.js
@@ -0,0 +1,27 @@
+function filterShortStateName(arr){
+    return arr.filter((item) => 
+        item.length >= 5
+    )
+}
+
+function filterStartVowel(ar){
+    return ar.filter((elm) => elm[0].match(/[eaiouEOUIA]/))
+}
+
+
+function filter5Vowels(arr){
+    return arr.filter((elm) => elm.match(/[aeiouAEIOU]/g).length >= 5)
+}
+
+
+
+function filter1DistinctVowel(arr) {
+  return arr.filter((elm) => {
+    const vwel = elm.toLowerCase().match(/[aeiou]/g);
+    return vwel && new Set(vwel).size === 1;
+  });
+}
+
+function multiFilter(arr){
+    return arr.filter((elm) => (elm.capital.length >5) && (((elm.name)[0].match(/[eaiouEOUIA]/g) || []).length === 0) && (((elm.tag).match(/[eaiouEOUIA]/g)|| []).length >= 1) && (elm.tag !== 'South'))
+}
