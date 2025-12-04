de3dcaa1d7999d7b9e8383dc7db168bf5e659c83
yelhamdi <yassin890890.890@gmail.com>
2025-12-04 12:02:31 +0100

    cron

--- match-cron.js
+++ match-cron.js
@@ -1,6 +1,5 @@
 function matchCron(cron , date){
     let minute = date.getMinutes()
-    console.log(minute);
     
     let month = date.getMonth()
     let hour = date.getHours()
@@ -8,7 +7,7 @@ function matchCron(cron , date){
     let dayW = date.getDay()
     
 
-    if ((cron[0]==minute || cron[0]=="*") && (cron[2]==hour || cron[2]=="*") && (cron[4]==dayM || cron[4]=="*") && (cron[6]==month || cron[6]=="*") && (cron[8]==(dayW+1) || cron[8]=="*")){
+    if ((cron[0]==minute || cron[0]=="*") && (cron[2]==hour || cron[2]=="*") && (cron[4]==dayM || cron[4]=="*") && (cron[6]==month || cron[6]=="*") && (cron[8]==(dayW) || cron[8]=="*")){
         return true
     }
     return false
