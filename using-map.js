e464664aa2f7087fe4583f0851a6f89380bc7a35
yelhamdi <yassin890890.890@gmail.com>
2025-12-04 16:08:13 +0100

    q1

--- using-map.js
+++ using-map.js
@@ -1,48 +1,54 @@
-function citiesOnly(arr){
-    let res = []
-    arr.map((elm) => {
-        res.push(elm.city)
-    })
-    return res
+function citiesOnly(arr) {
+  let res = [];
+  arr.map((elm) => {
+    res.push(elm.city);
+  });
+  return res;
 }
+function capitalize(str) {
+  let tmp = str.split(" ");
+   return tmp.map((elm) => {
+    return elm[0].toUpperCase() + elm.slice(1).toLowerCase();
+   
+  }).join(' ')
 
-function upperCasingStates(arr){
-    let res = []
-    arr.map((elm) => {
-        res.push(...[elm[0].toUpperCase() + elm.slice(1).toLowerCase() ])
-    })
-    return res
 }
+function upperCasingStates(arr) {
+  let arr2 = arr.map((elm) => {
+    return capitalize(elm);
+  });
 
-
-function fahrenheitToCelsius(arr){
-    let res =[]
-    arr.map((elm) => {
-        res.push(Math.round((((elm.split('°')[0]))-(32))*5/(9))+'°C')
-    })
-    return res 
-}
-
-function trimTemp(arr){
-    let res = []
-    arr.map((elm) => {
-        let x = elm.temperature.trim()
-        elm.temperature = x
-        res.push(elm)
-    })
-    return res
+  return arr2;
 }
 
 
-function fToC(F){
-    return Math.round((((F.split('°')[0]))-(32))*5/(9))+'°Celcius'
+function fahrenheitToCelsius(arr) {
+  let res = [];
+  arr.map((elm) => {
+    res.push(Math.round(((elm.split("°")[0] - 32) * 5) / 9) + "°C");
+  });
+  return res;
 }
-function tempForecasts(arr){
-    let res = []
-    arr.map((elm) => {
-         let x = elm.temperature
-        x = fToC(x)
-        res.push(x+' in '+ elm.city + ', '+ elm.state)
-    })
-    return res
+
+function trimTemp(arr) {
+  let res = [];
+  arr.map((elm) => {
+    let x = elm.temperature.trim();
+    elm.temperature = x;
+    res.push(elm);
+  });
+  return res;
+}
+
+function fToC(F) {
+  return Math.round(((F.split("°")[0] - 32) * 5) / 9) + "°Celcius";
+}
+function tempForecasts(arr) {
+  let res = [];
+  arr.map((elm) => {
+    let x = elm.temperature;
+    x = fToC(x);
+    res.push(x + " in " + elm.city + ", " + elm.state);
+  });
+  return res;
 }
