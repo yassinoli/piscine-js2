525b253ed47382f2c840c272fa7629be49f847d5
yelhamdi <yassin890890.890@gmail.com>
2025-12-04 10:37:17 +0100

    get-some-time

--- get-some-time.js
+++ get-some-time.js
@@ -1,6 +1,19 @@
-function firstDayWeek(week,year){
-    let date = new Date(year)
-    date.setDate(week*7)
-    return date 
+function firstDayWeek(weeks,year) {  
+    let format = year + '-01-01'
+    let date = new Date(format)
+    let dayOfWeek = date.getDay()
+    if (dayOfWeek ==0) {
+        dayOfWeek+=7
+    }
+    date.setDate(date.getDate()-(dayOfWeek-1) + (weeks-1)*7)
+    let dd = date.getDate() + ''
+    let mm = date.getMonth() + 1 + ''
+    let yyyy = date.getFullYear() + ''      
+    
+    
+    if (date.getFullYear() < year) {     
+    return '01-01-' + year
+    }
+    
+    return dd.padStart(2,'0') + '-' + mm.padStart(2,'0') + '-' + yyyy.padStart(4, '0')
 }
-
