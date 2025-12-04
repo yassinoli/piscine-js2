848e972d26b0d5f347f8a1c3b3df68c672721324
yelhamdi <yassin890890.890@gmail.com>
2025-12-04 14:13:10 +0100

    last

---
+++ day-of-the-year.js
@@ -0,0 +1,17 @@
+function dayOfTheYear(date){
+    let year =  date.getFullYear()
+    
+    let fstDate = new Date("0001-01-01")
+    if (date == fstDate){
+        return 1
+    }
+    
+    fstDate.setUTCFullYear(year)
+
+   let bfrlst = ((date.getTime() - fstDate.getTime()) / 1000 / 3600 / 24) +1
+   if (bfrlst >365){
+    return 1
+   } else{
+    return bfrlst
+   }
+}
