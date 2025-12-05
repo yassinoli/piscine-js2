91b7b022e5d25cbe3886052c90bced269884be08
yelhamdi <yassin890890.890@gmail.com>
2025-12-05 17:51:45 +0100

    quant

---
+++ quantifiers.js
@@ -0,0 +1,21 @@
+function every(arr,func){
+    for(let i=0 ; i<arr.length ; i++){
+        if (!func(arr[i])){
+            return false
+        }
+    }
+    return true
+}
+
+function some(arr,func){
+    for(let i=0 ; i<arr.length ; i++){
+        if (func(arr[i])){
+            return true
+        }
+    }
+    return false
+}
+
+function none(arr,func){
+    return !every(arr,func)
+}
