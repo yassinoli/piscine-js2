dee2b20ed64d41e2188bf4c854977d7d8a1f6d88
yelhamdi <yassin890890.890@gmail.com>
2025-12-05 15:14:43 +0100

    gg

--- mapper.js
+++ mapper.js
@@ -6,15 +6,18 @@ function map(arr,func){
     return res
 }
 
-function flatMap(arr, func){
-    let res = []
-    for(let i=0 ; i<arr.length ;i++){
-        let x = func(arr[i])
-        if (x instanceof Array){
-            res.push(...x)
-        }else{
-            res.push(x)
+
+function flatMap(arr, func) {
+    const res = [];
+    for (let i = 0; i < arr.length; i++) {
+        const x = func(arr[i], i, arr);
+        if (Array.isArray(x)) {
+            res.push(...x);
+        } else {
+            res.push(x);
         }
     }
-    return res
+    return res;
 }
+
+
