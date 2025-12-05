4d8a1823eedcf0b59e06f83951dad8ad671816bf
yelhamdi <yassin890890.890@gmail.com>
2025-12-05 15:39:47 +0100

    filter

---
+++ filter.js
@@ -0,0 +1,33 @@
+function filter(arr , func){
+    let res = []
+    for(let i = 0 ; i<arr.length ; i++){
+        if (func[aar[i],i,arr]){
+            res.push(arr[i])
+        }
+    }
+return res
+}
+
+function reject(arr,func){
+     let res = []
+    for(let i = 0 ; i<arr.length ; i++){
+        if (!func[aar[i],i,arr]){
+            res.push(arr[i])
+        }
+    }
+return res
+}
+
+function partition(arr,func){
+     let res = []
+     let tmp = []
+    for(let i = 0 ; i<arr.length ; i++){
+        if (func[aar[i],i,arr]){
+            res.push(arr[i])
+        }else{
+            tmp.push(arr[i])
+        }
+    }
+    res.push(tmp)
+return res
+}
