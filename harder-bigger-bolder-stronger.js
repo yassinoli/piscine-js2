db321a45fd4fbd456a27b6492c92fc2d8905032d
yelhamdi <yassin890890.890@gmail.com>
2025-12-06 16:27:26 +0100

    lose

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,23 +1,20 @@
-export function generateLetters(){
+export function generateLetters() {
     const L = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
-    for (let i=1 ; i<=120; i++){
-        const add = document.createElement("div")
-        add.append(L[Math.floor(Math.random()*100)%26])
-
-        add.style.fontSize = 11 + i + "px";
-        if (  i <=40 ){
+    for (let i = 1; i <= 120; i++) {
+        const add = document.createElement("div");
+        add.append(L[Math.floor(Math.random() * 26)])
+        add.style.fontSize = (10 + i) + "px"
+        if (i <= 40) {
             add.style.fontWeight = 300
         }
-        if (  i <=80 && i>40 ){
+        if (i > 40 && i <= 80) {
             add.style.fontWeight = 400
         }
-        if (  i <=120 && i>80 ){
+        if (i > 80 && i <= 120) {
             add.style.fontWeight = 600
         }
+
         document.body.append(add)
     }
-    
-    
 }
