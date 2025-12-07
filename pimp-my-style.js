a38089e38eec595009a3cfd79dc7acc79858baf1
yelhamdi <yassin890890.890@gmail.com>
2025-12-07 09:29:48 +0100

    pimpit

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -20,24 +20,30 @@ export const styles = [
 let idx = 0
 let turn = false
 
+
+
 export function pimp(){
     const elm = document.getElementsByClassName("button")
+
     if (!turn){
         elm[0].classList.remove("unpimp")
         elm[0].classList.add(styles[idx])
         idx++
-        if (idx === styles.length-1){
+        console.log(elm);
+        
+        if (idx === styles.length){
             turn = true
         }
     }else{
-        if (idx === styles.length-1){
+        if (idx === styles.length){
             elm[0].classList.add("unpimp")
         }
-        elm[0].classList.remove(styles[idx])
         idx--
         if (idx === 0){
             turn = false
         }
-    }
-}
+                elm[0].classList.remove(styles[idx])
 
+    }
+
+}
