4fd2729736eb1992f0ea16d54f11524c0b486e72
yelhamdi <yassin890890.890@gmail.com>
2025-12-07 09:16:44 +0100

    pimpit

---
+++ pimp-my-style.js
@@ -0,0 +1,43 @@
+
+export const styles = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'ten',
+  'eleven',
+  'twelve',
+  'thirteen',
+  'fourteen',
+  'fifteen',
+]
+
+let idx = 0
+let turn = false
+
+export function pimp(){
+    const elm = document.getElementsByClassName("button")
+    if (!turn){
+        elm[0].classList.remove("unpimp")
+        elm[0].classList.add(styles[idx])
+        idx++
+        if (idx === styles.length){
+            turn = true
+        }
+    }else{
+        if (idx === styles.length){
+            elm[0].classList.add("unpimp")
+        }
+        elm[0].classList.remove(styles[idx])
+        idx--
+        if (idx === 0){
+            turn = false
+        }
+    }
+}
+
