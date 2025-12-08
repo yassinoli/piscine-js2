d8065c41a371df973bb670ce49295aef34726de0
yelhamdi <yassin890890.890@gmail.com>
2025-12-08 12:23:51 +0100

    pick

---
+++ pick-omit.js
@@ -0,0 +1,20 @@
+function pick(obj , ...str){
+    let res = {}
+    for(key of Object.keys(obj)){
+        if (str.includes(key)){
+            res[key] = obj[key]
+        }
+    }
+    return res
+}
+
+function omit(obj , ...str){
+let res = {}
+    for(key of Object.keys(obj)){
+        if (!str.includes(key)){
+            res[key] = obj[key]
+        }
+    }
+    return res
+}
+
