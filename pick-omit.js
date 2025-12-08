a733ff6b9943efa56779bc9137e90665f96568ea
yelhamdi <yassin890890.890@gmail.com>
2025-12-08 12:34:54 +0100

    pick

--- pick-omit.js
+++ pick-omit.js
@@ -3,24 +3,43 @@ function pick(obj , str){
 
     let res = {}
 
+    if (Array.isArray(str)){
          for(let key of Object.keys(obj)){
         if (str.includes(key)){
             res[key] = obj[key]
         }
     }
     return res
-   
+    }else{
+        for(let key of Object.keys(obj)){
+        if (str == key){
+            res[key] = obj[key]
+        }
+    }
+    return res
+    }
 
     
 }
 
 function omit(obj , str){
-let res = {}
-    for(let key of Object.keys(obj)){
+ let res = {}
+
+    if (Array.isArray(str)){
+         for(let key of Object.keys(obj)){
         if (!str.includes(key)){
             res[key] = obj[key]
         }
     }
     return res
+    }else{
+        for(let key of Object.keys(obj)){
+        if (str != key){
+            res[key] = obj[key]
+        }
+    }
+    return res
+    }
 }
 
+console.log(pick({ha:2 , a:3},["h" , "a"]));
