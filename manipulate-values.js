d6b38a41cccad60d319f921244b664ba0de4577a
yelhamdi <yassin890890.890@gmail.com>
2025-12-09 14:21:09 +0100

    man

--- manipulate-values.js
+++ manipulate-values.js
@@ -30,14 +30,3 @@ function reduceValues(obj , func){
 
 
 
-const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
-
-console.log(filterValues(nutrients, (nutrient) => nutrient <= 12))
-// output: { carbohydrates: 12, fat: 5 }
-
-console.log(mapValues(nutrients, (v) => v+1))
-// output: { carbohydrates: 13, protein: 21, fat: 6 }
-
-console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
-// output: 37
-
