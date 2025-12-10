a7bd8b0296f525f2debce54f4a764678dc443258
yelhamdi <yassin890890.890@gmail.com>
2025-12-10 20:07:22 +0100

    prn

--- pronoun.js
+++ pronoun.js
@@ -1,6 +1,6 @@
 function pronoun(str) {
   const arr = ['i', 'you', 'he', 'she', 'it', 'they', 'we']
-  const w = str.split(/\s+|[.]|[,]/)
+  const w = str.split.toLowercase()(/\s+|[.]|[,]/)
   const res = {}
 
   for (let i = 0; i < w.length; i++) {
@@ -19,3 +19,4 @@ function pronoun(str) {
 
   return res
 }
+
