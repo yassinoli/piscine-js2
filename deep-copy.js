d4b881c3c905310bcc46bab175c08b0b6950d14f
yelhamdi <yassin890890.890@gmail.com>
2025-12-10 16:49:22 +0100

    dc

---
+++ deep-copy.js
@@ -0,0 +1,22 @@
+function deepCopy(obj){
+    let res 
+    if (Array.isArray(obj)){
+        for(let i=0 ; i<obj.length; i++){
+            if (obj[i] instanceof Array){
+                res.push(deepCopy(obj[i]))
+            }else{
+                res.push(obj[i])
+            }
+        }
+    }else{
+        for(ky of Object.keys(obj)){
+            if (typeof obj[ky] === 'object'){
+               res[ky] = deepCopy(obj[ky])
+            }else{
+                res[ky] = obj[ky]
+            }
+        }
+    }
+    return res
+
+}
