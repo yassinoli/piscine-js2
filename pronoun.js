b700d133432b7f54a9c65deb3b9ef3d5014bb878
yelhamdi <yassin890890.890@gmail.com>
2025-12-10 19:54:37 +0100

    prn

---
+++ pronoun.js
@@ -0,0 +1,23 @@
+function pronoun(str) {
+  const arr = ['i', 'you', 'he', 'she', 'it', 'they', 'we']
+  const w = str.split(' ')
+  const res = {}
+
+  for (let i = 0; i < w.length; i++) {
+    
+    if (arr.includes(w[i])) {
+      if (!res[w[i]]) {
+        res[w[i]] = { word: [], count: 0 }
+      }
+      res[w[i]].count++
+      const next = w[i + 1]
+      if (next && !arr.includes(next)) {
+        res[w[i]].word.push(next)
+      }
+    }
+  }
+
+  return res
+}
+
+
