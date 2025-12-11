2eed61793a60e2d7139fd2fbec77dad3bed65c94
yelhamdi <yassin890890.890@gmail.com>
2025-12-11 17:12:05 +0100

    debounce

---
+++ debounce.js
@@ -0,0 +1,34 @@
+function debounce(func , t){
+    let r
+   return function(...args){ 
+    clearTimeout (r)
+    r =  setTimeout(() => {
+      return  func(args)
+    }, t);}
+}
+
+function opDebounce(func , wait , {leading = false} = {}){
+  let tim
+  let clead = false
+
+    return function(...args){ 
+      const ctx = this
+      if (leading && !tim){
+        function aply(ctx , ...args){
+          clead = true
+          tim = setTimeout(() => {
+            tim = null
+            clead = false
+          }, wait);
+        }
+      }
+    clearTimeout (tim)
+    tim =  setTimeout(() => {
+      if (!leading || !clead){
+        func.aply(ctx,args)
+      }
+      tim = null
+      clead = false
+    }, t);}
+}
+
