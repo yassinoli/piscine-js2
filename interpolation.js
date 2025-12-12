a34ab7b3893507deedf7008b8d2aa75666a09ea3
yelhamdi <yassin890890.890@gmail.com>
2025-12-12 13:17:43 +0100

    -

--- interpolation.js
+++ interpolation.js
@@ -1,10 +1,11 @@
 function interpolation({ step, start, end, callback, duration }) {
-    const distStep = (end - start) / step 
-    const timeStep = duration / step         
+    const distStep = (end - start) / step
+    const timeStep = duration / step
+
     for (let i = 1; i <= step; i++) {
         setTimeout(() => {
-            const x = start + distStep * i     
-            const y = timeStep * i            
+            const x = start + distStep * (i - 1)
+            const y = timeStep * i
             callback([x, y])
         }, timeStep * i)
     }
