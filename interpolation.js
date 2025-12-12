c6a1c7259719430c030e4b8fc2d6395d9ee3fb0e
yelhamdi <yassin890890.890@gmail.com>
2025-12-12 13:10:31 +0100

    -

--- interpolation.js
+++ interpolation.js
@@ -2,7 +2,7 @@ function interpolation(option = {}){
     let dist = (option.end - option.start) / option.step
     let point = option.duration/option.step
   
- for (let i = 1 ; i<option.step ; i++) {
+ for (let i = 1 ; i<=option.step ; i++) {
         setTimeout(() => {
             const x= option.start + dist*i
            const  y = option.point*i
