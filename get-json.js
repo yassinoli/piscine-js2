4135458ea4e7b6dab5d1e69d5f83f5b330e5fe13
yelhamdi <yassin890890.890@gmail.com>
2025-12-13 12:09:34 +0100

    (

--- get-json.js
+++ get-json.js
@@ -1,25 +1,21 @@
-async function getJSON(path , params){
+async function getJSON(path, params) {
     let url = path
-    if (params){
-     url =  `${path}`+ new URLSearchParams(params).toString()
-    }
-    
-   let  response = await fetch(url)
-  
-        
-        if (!response.ok){
-            throw new Error(response.statusText)
-        }else{
-            let data =  await response.json()
-            if (data.data){
-                return data.data
-            }else{
-               throw new Error(data.error) 
-            }
-        }
-       
-    
-   
-   
 
+    if (params) {
+        url = `${path}?${new URLSearchParams(params).toString()}`
+    }
+
+    const response = await fetch(url)
+
+    if (!response.ok) {
+        throw new Error(response.statusText)
+    }
+
+    const data = await response.json()
+
+    if (data.data) {
+        return data.data
+    } else {
+        throw new Error(data.error)
+    }
 }
