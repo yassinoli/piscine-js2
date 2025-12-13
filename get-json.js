41fc1716a12290b78aaae390de83f2a38adaa638
yelhamdi <yassin890890.890@gmail.com>
2025-12-13 12:05:16 +0100

    (

---
+++ get-json.js
@@ -0,0 +1,25 @@
+async function getJSON(path , params){
+    let url = path
+    if (params){
+     url =  `${path}`+ URLSearchParams(params).toString()
+    }
+    
+   let  response = await fetch(url)
+  
+        
+        if (!response.ok){
+            throw new Error(response.statusText)
+        }else{
+            let data =  await response.json()
+            if (data.data){
+                return data.data
+            }else{
+               throw new Error(data.error) 
+            }
+        }
+       
+    
+   
+   
+
+}
