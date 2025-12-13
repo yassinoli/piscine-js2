859cfb7bb3503a228ee06f749739da8fa2c0c438
yelhamdi <yassin890890.890@gmail.com>
2025-12-13 16:49:42 +0100

    promiseAll

---
+++ gougle-search.js
@@ -0,0 +1,28 @@
+async function queryServers(serverName , q){
+    let fst = await getJSON(`/pouet?q=${q}`)
+    let snd = await getJSON(`/pouet_backup?q=${q}`)
+    return  Promise.race([fst , snd])
+}
+
+function gougleSearch(q){
+
+    let allprom = Promise.all([ queryServers('web' , q), queryServers('web' , q), queryServers('web' , q) ])
+    .then((value)=>{
+        return { web : value[0] , image : value[1] , video : value[2]}
+    })
+     let timout = new Promise((res , rej)=>{
+        setTimeout(() => {
+            rej(Error('timeoute'))
+        }, 80);
+     })
+    return Promise.race([allprom , timout])
+     
+}
+
+
+
+
+
+
+
+
