25092062f5173160656a86580ec84a7694f197d4
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 15:22:12 +0100

    ---

---
+++ tell-me-vip.mjs
@@ -0,0 +1,35 @@
+import { writeFile } from 'node:fs/promises';
+import { readdir } from 'fs/promises';
+import { readFile }  from 'fs/promises';
+
+let dir = await readdir(process.argv[2])
+
+let yessed = []
+ for(let elm of dir){
+   let tmp = await readFile(`${process.argv[2]}/${elm}`, 'utf8')
+   
+  let an =  JSON.parse(tmp)
+  if(an.answer === 'yes'){
+    yessed.push(elm + ' ')
+  }
+}
+
+ let res = []
+yessed.forEach((elm) => {
+    res.push((elm.split('_').join(' ').replace(".json",'')))
+});
+
+let result = []
+res.forEach(element => {
+    result.push(element.split(' ').reverse().join(' '))
+});
+result = result.sort()
+
+result.forEach((element , idx) => {
+   console.log( ((idx+1 + '. ' + element )));
+  
+});
+
+writeFile('vip.txt' ,result.map((item,idx) => {
+    return idx+1 + '. ' + item + '\n'
+}))
