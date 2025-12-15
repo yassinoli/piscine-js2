0e22ab8561c8894dc7296bcce5d77ece2b27b210
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 16:26:11 +0100

    ---

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -8,10 +8,10 @@ let arg3 = process.argv[4]
 
 let file = await readFile(arg1 , 'utf8')
 if (arg2==='decode'){
-    writeFile(arg3 ?? 'clear.txt' , Buffer.from(file).toString('base64') )
+  await  writeFile(arg3 ?? 'clear.txt' , Buffer.from(file).toString('base64') )
 }
 if (arg2==='encode'){
-    writeFile(arg3 ?? 'cypher.txt', Buffer.from(file, 'base64' ).toString('utf8'))
+  await  writeFile(arg3 ?? 'cypher.txt', Buffer.from(file, 'base64' ).toString('utf8'))
 }
 
  
