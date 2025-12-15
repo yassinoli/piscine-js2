42d102a6cc0db4fef487862dd8c5c676dbbe5bde
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 12:35:37 +0100

    -

---
+++ verydisco.mjs
@@ -0,0 +1,15 @@
+const args = process.argv.slice(2)
+
+let spl = args
+
+let res = []
+
+
+
+spl.forEach(elm => {
+    let tmp = ""
+    tmp+= elm.slice(Math.ceil(elm.length/2))
+    tmp+= elm.slice(0,Math.ceil(elm.length/2))
+    res.push(tmp)
+});
+    console.log(res.join(' '))
