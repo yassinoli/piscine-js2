fcc3a9a7d255362093ece4c01d30f53b8e8b4cd4
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 15:55:59 +0100

    ---

---
+++ tell-it-cypher.mjs
@@ -0,0 +1,12 @@
+import { readFile }  from 'fs/promises';
+import { writeFile } from 'node:fs/promises';
+
+let arg1 = process.argv[2]
+let arg2 = process.argv[3]
+let file = readFile(arg1)
+if (arg2==='decode'){
+    writeFile('clear.txt' , file.from() )
+}
+if (arg2==='encode'){
+    writeFile('cypher.txt' , file.toString() )
+}
