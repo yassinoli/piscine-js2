6c4d9ceba13b57502fa7dea573a91c148f45e540
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 13:44:58 +0100

    ---

---
+++ tell-me-who.mjs
@@ -0,0 +1,13 @@
+import { readdir } from 'node:fs/promises';
+
+let args = process.argv[2]
+
+let rdd = await  readdir(args)
+
+let res = []
+
+rdd.forEach((elm , idx) => {
+    res.push(idx+1 + '. ' + elm.split('_').join(' ').split('.').slice(0, -1))
+});
+
+console.log(res.join('\n'));
