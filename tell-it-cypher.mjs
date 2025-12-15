1c9e7c4eceb7996e35ed8c8a3d01952491dc123d
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 15:59:17 +0100

    ---

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -1,12 +1,14 @@
 import { readFile }  from 'fs/promises';
 import { writeFile } from 'node:fs/promises';
+import { Buffer } from 'node:buffer';
+
 
 let arg1 = process.argv[2]
 let arg2 = process.argv[3]
 let file = await readFile(arg1)
 if (arg2==='decode'){
-    writeFile('clear.txt' , file.from() )
+    writeFile('clear.txt' , Buffer.from(file).from() )
 }
 if (arg2==='encode'){
-    writeFile('cypher.txt' , file.toString() )
+    writeFile('cypher.txt' , file.toString('utf8') )
 }
