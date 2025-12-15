2e581cbdcb8870292fd23491139a7bf57a8d396a
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 12:50:53 +0100

    -

--- verydisco.mjs
+++ verydisco.mjs
@@ -5,7 +5,7 @@ let spl = args[0]
 let res = []
 
 
-spl.forEach(element => {
+spl.split(' ').forEach(elm => {
     let tmp = ""
     tmp+= elm.slice(Math.ceil(elm.length/2))
     tmp+= elm.slice(0,Math.ceil(elm.length/2))
