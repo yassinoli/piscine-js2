2529273dafe5f4b800bac91a19826cc4614009c5
yelhamdi <yassin890890.890@gmail.com>
2025-12-15 13:51:31 +0100

    ---

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -7,7 +7,7 @@ let rdd = await  readdir(args)
 let res = []
 
 rdd.forEach((elm , idx) => {
-    res.push(idx+1 + '. ' + elm.split('_').join(' ').split('.').slice(0, -1))
+    res.push(idx+1 + '. ' + (elm.split('_').join(' ').split('.')).slice(0, -1).split(' ').reverse().join(' '))
 });
 
 console.log(res.join('\n'));
